1979da7f0fddafe857e74feff5c035946e6d75ef
Gregory Coletta <gregory.coletta@here.com>
2014-04-11 17:26:03 -0400

    Adding scbedroid, gson and writing to scbe.

--- src/com/here/assemble/MainActivity.java
+++ src/com/here/assemble/MainActivity.java
@@ -1,22 +1,51 @@
 package com.here.assemble;
 
+import com.nokia.scbe.droid.LocalBinder;
+import com.nokia.scbe.droid.ScbeService;
+import com.nokia.scbe.droid.ScbeClient.ScbeEnvironment;
+
 import android.app.Activity;
-import android.app.Fragment;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class MainActivity extends Activity {
+	 private static final String TAG = "ASSEMBLE.MainActivity";
+	 private ScbeService mScbeService = null;
 
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @SuppressWarnings("unchecked")
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            Log.d(TAG, "onServiceConnected");
+            mScbeService = ((LocalBinder<ScbeService>) service).getService();
+
+            // do any extra setup that requires your Service
+            mScbeService.setScbeEnvironment(ScbeEnvironment.QAEnvironment);
+            mScbeService.setUserId("AssembleDemoUser");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName className) {
+            Log.d(TAG, "onServiceDisconnected");
+        }
+    };
+	 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+        // bind to our database using our Service Connection
+        Intent serviceIntent = new Intent(this, ScbeService.class);
+        bindService(serviceIntent, mConnection, Context.BIND_AUTO_CREATE);
+		
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new AssembleFragment()).commit();
@@ -41,5 +70,13 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public ScbeService getScbeService() {
+        if (mConnection != null) {
+            return mScbeService;
+        } else {
+            return null;
+        }
 	}
 }
